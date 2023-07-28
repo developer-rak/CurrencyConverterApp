19da90f0a17a4759919a2aabf70ea157ec0f3c9e
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2023-07-28 16:44:45 +0500

    init

--- script.js
+++ script.js
@@ -2,8 +2,17 @@ const dropList = document.querySelectorAll(".drop-list select");
 
 for (let i = 0; i < dropList.length; i++) {
     for (currency_code in country_code) {
+        // selecting USD by default as FROM currency and PKR as TO currency
+        let selected;
+        if (i == 0) {
+            selected = currency_code == "USD" ? "selected" : "";
+        } else {
+            if (i == 1) {
+                selected = currency_code == "PKR" ? "selected" : "";
+            }
+        }
         // // creating option tag with passong currency code as a text and value
-        let optionTag = `<option value="${currency_code}">${currency_code}</option>`;
+        let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
         // // inserting option tag inside select tag
         dropList[i].insertAdjacentHTML("beforeend", optionTag);
     }
