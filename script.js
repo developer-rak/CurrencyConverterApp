e4cfaad2bc737f35e9057627884a7c9f9748290b
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2023-08-01 10:24:14 +0500

    finilize

--- script.js
+++ script.js
@@ -19,11 +19,38 @@ for (let i = 0; i < dropList.length; i++) {
         // // inserting option tag inside select tag
         dropList[i].insertAdjacentHTML("beforeend", optionTag);
     }
+    dropList[i].addEventListener("change", e => {
+        loadFlag(e.target); // calling loadflag with passing target element as an argument
+    });
 }
+
+function loadFlag(element) {
+    for (code in country_code) {
+        if (code == element.value) { // if currency code of country list is equal to option value
+            let imgTag = element.parentElement.querySelector("img"); // selecting img tag of particular drop list
+            // passing countru code of a selected currency code in a img url
+            imgTag.src = `https://flagsapi.com/${country_code[code]}/flat/64.png`
+        }
+    }
+}
+
+window.addEventListener("load", () => {
+    getExchangeRate();
+});
 
 getButton.addEventListener("click", e => {
     e.preventDefault(); // preventing form from submitting
     getExchangeRate();
+});
+
+const exchangeIcon = document.querySelector(".drop-list .icon");
+exchangeIcon.addEventListener("click", () => {
+    let tempCode = fromCurrency.value; // temporary currency code of FROM drop list
+    fromCurrency.value = toCurrency.value; // passing TO currency code to FROM currency code
+    toCurrency.value = tempCode; // passing temporary currency code to TO currency code
+    loadFlag(fromCurrency); // calling loadflag with passing select element (fromCurrency) of FROM
+    loadFlag(toCurrency); // calling loadflag with passing select element (toCurrency) of TO
+    getExchangeRate()
 });
 
 function getExchangeRate() {
@@ -43,5 +70,7 @@ function getExchangeRate() {
         let exchangeRate = result.conversion_rates[toCurrency.value];
         let totalExchangeRate = (amountVal * exchangeRate).toFixed(2);
         exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExchangeRate} ${toCurrency.value}`;
-    })
+    }).catch(() => { // if user is offline or any other error occured while fetching data then catch function will run
+        exchangeRateTxt.innerText = "Something went wrong";
+    });
 }
